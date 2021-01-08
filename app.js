dcfc1f3d823d0e33c51406ef8cf7532bc37029b7
adard2002 <adard2002@gmail.com>
2021-01-08 15:01:00 -0600

    question one passes

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 'use strict';
+console.log('connected');
 /////////////////////////////////////
 /* Problem 1 (this is your demo that we'll solve in class)
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
@@ -7,17 +8,19 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
+  var theSum = a + b;
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + theSum + '.';
+  return [theSum, message];
+
 
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
-
-
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 "The product of 5 and 9 is 45."
@@ -32,8 +35,6 @@ function multiply(a, b) { //eslint-disable-line
 // testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-
-
 
 /////////////////////////////////////
 /* Problem 3
@@ -52,8 +53,6 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 // testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-
-
 
 /////////////////////////////////////
 /* Problem 4
--- tests.js
+++ tests.js
@@ -25,3 +25,27 @@ function testSumAndMultiply() { //eslint-disable-line
     console.log('%c TEST FOR sumAndMultiply() FAILS', 'color: red');
   }
 }
+
+function testSumArray() { //eslint-disable-line
+  if (sumArray(testArray)[0] === 9 && sumArray(testArray)[1] === '2,3,4 was passed in as an array of numbers, and 9 is their sum.') {
+    console.log('%c TEST FOR sumArray() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR sumArray() FAILS', 'color: red');
+  }
+}
+
+function testMultiplyArray() { //eslint-disable-line
+  if (multiplyArray(testArray)[0] === 24 && multiplyArray(testArray)[1] === 'The numbers 2,3,4 have a product of 24.') {
+    console.log('%c TEST FOR multiplyArray() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiplyArray() FAILS', 'color: red');
+  }
+}
+
+function testMultiplyAnyArray() { //eslint-disable-line
+  if (multiplyAnyArray(testDynamicArray)[0] === 120 && multiplyAnyArray(testDynamicArray)[1] === 'The numbers 1,2,3,4,5 have a product of 120.') {
+    console.log('%c TEST FOR multiplyAnyArray() PASSES', 'color: green');
+  } else {
+    console.log('%c TEST FOR multiplyAnyArray() FAILS', 'color: red');
+  }
+}
